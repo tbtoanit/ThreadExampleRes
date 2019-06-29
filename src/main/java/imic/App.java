ba878d229545c0ece47fb76e62640f9fafcd1e34
Trần Bảo Toàn <49049899+tbtoanit@users.noreply.github.com>
2019-06-29 17:16:55 +0700

    Add files via upload

---
+++ src/main/java/imic/App.java
@@ -0,0 +1,14 @@
+package imic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //Edit the v1.1
+        System.out.println( "Hello World!" );
+    }
+}
